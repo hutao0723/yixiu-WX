74517ce34b79ae3d9ffe4a4fc8a5717b8de70539
liugaoshuai <liugaoshuai@duiba.com.cn>
2018-05-15 20:32:35 +0800

    ahe

--- src/api/order.js
+++ src/api/order.js
@@ -70,7 +70,7 @@ export default class order extends base {
       // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       debug: true,
       // 必填，公众号的唯一标识
-      appId: obj.appId,
+      appId: obj.appid,
       // 必填，生成签名的时间戳
       timestamp: obj.timestamp,
       // 必填，生成签名的随机串
@@ -122,7 +122,7 @@ export default class order extends base {
       // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       debug: true,
       // 必填，公众号的唯一标识
-      appId: obj.appId,
+      appId: obj.appid,
       // 必填，生成签名的时间戳
       timestamp: obj.timestamp,
       // 必填，生成签名的随机串
@@ -174,7 +174,7 @@ export default class order extends base {
       // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       debug: true,
       // 必填，公众号的唯一标识
-      appId: obj.appId,
+      appId: obj.appid,
       // 必填，生成签名的时间戳
       timestamp: obj.timestamp,
       // 必填，生成签名的随机串
