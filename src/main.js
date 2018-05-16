749a6f02821f4a7134e7397459e8789f7951ce8a
liugaoshuai <liugaoshuai@duiba.com.cn>
2018-05-16 15:24:55 +0800

    ahe

--- src/main.js
+++ src/main.js
@@ -71,9 +71,9 @@ Vue.prototype.wxShare = function (title, desc, link) {
     wx.config({
       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: Data.appid, // 必填，公众号的唯一标识
-      timestamp: Data.timestamp, // 必填，生成签名的时间戳
+      timestamp: Number(Data.timestamp), // 必填，生成签名的时间戳
       nonceStr: Data.noncestr, // 必填，生成签名的随机串
-      signature: Number(Data.signature), // 必填，签名，见附录1
+      signature: Data.signature, // 必填，签名，见附录1
       jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline', 'previewImage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
   });
