cde5a4d58bdab832ac9ccbea5d6e9d7f7463ca1d
liugaoshuai <liugaoshuai@duiba.com.cn>
2018-05-16 15:07:02 +0800

    ahe

--- src/main.js
+++ src/main.js
@@ -47,11 +47,70 @@ Vue.http.interceptors.push((request, next) => {
     // response.body = '...';
     if (response.data.code == '000001') {
       // const url = '/' + window.location.href.split('/').slice(3).join('/');
-      location.href = "http://k.youfen666dev.com/loginH5?dbredirect=/home/index";
+      location.href = "/loginH5?dbredirect=/#/home/index";
     }
     return response;
   });
 });
+
+Vue.prototype.wxShare = function (title, desc, link) {
+  // var link  = encodeURIComponent(link);
+  // const url = encodeURIComponent(location.href.split('#')[0]);// 当前url
+  // const url = encodeURIComponent(window.location.href.split('#')[0]);
+  // const url = location.href.split('#')[0];
+  const urlData = `/wechat/getJsapiSignature`;
+  const url = location.href;
+
+  Vue.http.get(urlData, {
+    params: {
+      url: url
+    }
+  }).then(res => {// 获得签名配置
+    var Data = res.data.data;
+    // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，
+    wx.config({
+      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: Data.appid,   // 必填，公众号的唯一标识
+      timestamp: Data.timestamp, // 必填，生成签名的时间戳
+      nonceStr: Data.nonceStr,   // 必填，生成签名的随机串
+      signature: Data.signature, // 必填，签名，见附录1
+      jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    });
+  });
+  wx.ready(() => {
+    // 所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，
+    // 则可以直接调用，不需要放在ready函数中。
+    wx.onMenuShareAppMessage({ // 分享给朋友
+      title: title, // 分享标题
+      desc: desc,   // 分享描述
+      link: link,   // 分享链接 默认以当前链接
+      imgUrl: '',// 分享图标
+      // 用户确认分享后执行的回调函数
+      success: function () {
+        
+      },
+      // 用户取消分享后执行的回调函数
+      cancel: function () {
+        console.log('分享到朋友取消');
+      }
+    });
+    //分享到朋友圈
+    wx.onMenuShareTimeline({
+      title: title, // 分享标题
+      desc: desc,
+      link: link,
+      imgUrl: '',
+      // 用户确认分享后执行的回调函数
+      success: function () {
+        
+      },
+      // 用户取消分享后执行的回调函数
+      cancel: function () {
+        console.log('分享到朋友圈取消');
+      }
+    });
+  });
+};
 
 router.beforeEach((to, from, next) => {
   /* 路由发生变化修改页面title */
--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -20,83 +20,92 @@
 </template>
 
 <script>
-import order from '../api/order';
-import AudioBar from 'components/basic/Audio_Bar';
-export default {
-  components: { AudioBar },
-  data () {
-    return {
-      data: {},
-      imageUrl: 'https://yun.dui88.com/yoofans/images/201804/miniapp/help-center.png',
-      username: '一修课堂'
-    };
-  },
-  watch: {},
-  async mounted () {
-    const arr = ['previewImage','onMenuShareAppMessage','onMenuShareTimeline']
-    const res = await order.getApiRules(arr);
-    
-  },
-  methods: {
-    // 预览
-    onImage(){
-      order.wxPreview();
+  import order from '../api/order';
+  import AudioBar from 'components/basic/Audio_Bar';
+  export default {
+    components: {
+      AudioBar
+    },
+    data() {
+      return {
+        data: {},
+        imageUrl: 'https://yun.dui88.com/yoofans/images/201804/miniapp/help-center.png',
+        username: '一修课堂'
+      };
+    },
+    watch: {},
+    created() {
+      var link = window.location.href;
+      this.wxShare('这一次,我想和你一起走在美丽的路上', '我正在参加中国丽人秀海选盛典,诚请你为我投上宝贵的一票', link)
+    },
+    mounted() {
+      // const arr = ['previewImage', 'onMenuShareAppMessage', 'onMenuShareTimeline']
+      // const res = await order.getApiRules(arr);
+
+    },
+    methods: {
+      // 预览
+      onImage() {
+        order.wxPreview();
+      }
     }
-  }
-};
+  };
+
 </script>
 
 <style lang="less">
-@import '../less/tool.less';
-.help-box{
+  @import '../less/tool.less';
+  .help-box {
     height: 100%;
     padding: 1/@rem 0;
-    box-sizing:border-box;
+    box-sizing: border-box;
     border-top: 7/@rem solid #F4F4F4;
-    .info-icon{
+    .info-icon {
       height: 150/@rem;
       margin: 97/@rem auto 45/@rem;
       width: 150/@rem;
       display: block;
       border-radius: 50%;
     }
-    .info-text{
+    .info-text {
       text-align: center;
       .fontSize(36);
-      color:rgba(34,34,34,1);
+      color: rgba(34, 34, 34, 1);
       margin-bottom: 74/@rem;
     }
-    .word{
+    .word {
       .fontSize(28);
-      color:rgba(102,102,102,1);
-      text-align:center;
+      color: rgba(102, 102, 102, 1);
+      text-align: center;
       line-height: 40/@rem;
     }
-    .introduce{
+    .introduce {
       margin: 55/@rem auto 20/@rem;
     }
-    .btn-box{
-      padding-top:55/@rem;
-      .btn-find{
-        width:360/@rem;
-        height:70/@rem; 
-        border-radius: 34/@rem; 
+    .btn-box {
+      padding-top: 55/@rem;
+      .btn-find {
+        width: 360/@rem;
+        height: 70/@rem;
+        border-radius: 34/@rem;
         line-height: 70/@rem;
         margin: 0 auto;
         .fontSize(30);
-        display:block;
-        color: rgba(255,80,72,1);
+        display: block;
+        color: rgba(255, 80, 72, 1);
         background: inherit;
-        margin-bottom:55/@rem;
-        border: 2/@rem solid rgba(255,80,72,1);
-        &.bgred{
-          background:linear-gradient(-90deg,rgba(255,80,72,1),rgba(255,80,72,1));
-          box-shadow: 0 10/@rem 14/@rem 0 rgba(255,209,209,0.5);
-          color:rgba(255,255,255,1);
+        margin-bottom: 55/@rem;
+        border: 2/@rem solid rgba(255, 80, 72, 1);
+        &.bgred {
+          background: linear-gradient(-90deg, rgba(255, 80, 72, 1), rgba(255, 80, 72, 1));
+          box-shadow: 0 10/@rem 14/@rem 0 rgba(255, 209, 209, 0.5);
+          color: rgba(255, 255, 255, 1);
           border-color: transparent;
           border: 0;
         }
       }
     }
   }
+
 </style>
+
