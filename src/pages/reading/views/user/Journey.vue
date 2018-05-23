546f58089b15f62ff9339ca3eebb8d8de737b0da
jiaxiaochen <jiaxiaochen@duiba.com>
2018-05-23 10:50:00 +0800

    讲师我的客服

---
+++ src/pages/reading/views/user/Journey.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="lecturer-main">
+    <div class="title">老师微信信息</div>
+    <img class="code" :src="codeUrl">
+    <div class="name">{{name}}</div>
+    <div class="point">
+      温馨提示：由于微信对添加好友的限制，老师可能收不到你的好友请求，如果在三个工作日内老师还未通过你的好友申请，你可以再发一次哦！
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import bnav from '../../components/basic/Nav';
+export default {
+  components: {
+    bnav
+  },
+  data () {
+    return {
+      data: {},
+      name: '哈哈',
+      codeUrl: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=834282135,3059144775&fm=27&gp=0.jpg'
+    };
+  },
+  computed: {
+    ...mapState({})
+  },
+  created() {
+    },
+  mounted () {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="less">
+@import '../../less/variable';
+@import '../../less/base';
+@import '../../less/icon';
+.lecturer-main{
+  margin: 70/@rem 0;
+  .title{
+    text-align: center;
+    .fontSize(40);
+    font-weight: bold;
+    margin-bottom: 58/@rem;
+  }
+  .code{
+    margin:0 auto 30/@rem;
+    width: 524/@rem;
+    height: 524/@rem;
+    display: block;
+    border: 1px solid #aaa;
+  }
+  .name{
+    font-weight: bold;
+    .fontSize(44);
+    text-align: center;
+    margin-bottom: 64/@rem;
+  }
+  .point{
+    width:630/@rem;
+    .fontSize(28);
+    color: @color-major;
+    margin:0 auto;
+    line-height: 40/@rem;
+  }
+}
+
+  
+</style>
+
