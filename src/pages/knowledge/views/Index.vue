db0d6d566b7882447f41fc59bc517c4c0e79cd59
liugaoshuai <liugaoshuai@duiba.com.cn>
2018-05-24 14:09:36 +0800

    ahe

--- src/pages/knowledge/views/Index.vue
+++ src/pages/knowledge/views/Index.vue
@@ -23,7 +23,7 @@ export default {
     const msg = {
             title: '一修读书', // 分享标题
             desc: '在这里发现更好的自己', // 分享描述
-            link: 'http://k.youfen666dev.com/knowledge.html#/index/home', // 分享链接 默认以当前链接
+            link: 'http://k.youfen666test.com/knowledge.html#/index/home', // 分享链接 默认以当前链接
             imgUrl: 'https://yun.duiba.com.cn/yoofans/images/201804/miniapp/knowledge.jpg', // 分享图标
           }
 
