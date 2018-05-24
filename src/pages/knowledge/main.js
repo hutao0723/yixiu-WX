3327c4262102e3c5958728938d3a0d63d88ff5e9
liugaoshuai <liugaoshuai@duiba.com.cn>
2018-05-24 10:52:26 +0800

    ahe

--- src/pages/knowledge/main.js
+++ src/pages/knowledge/main.js
@@ -73,13 +73,16 @@ Vue.prototype.wxShare = function (msg) {
   // const url = encodeURIComponent(window.location.href.split('#')[0]);
   // const url = location.href.split('#')[0];
 
+  let obj = {}
   if (!msg) {
-    msg = {
+    obj = {
       title: '一修读书', // 分享标题
       desc: '在这里发现更好的自己', // 分享描述
       link: 'http://k.youfen666dev.com/knowledge.html#/index/home', // 分享链接 默认以当前链接
       imgUrl: 'https://yun.duiba.com.cn/yoofans/images/201804/miniapp/knowledge.jpg', // 分享图标
     }
+  }else{s
+    obj = msg
   }
   const urlData = `/wechat/getJsapiSignature`;
   const url = location.href.split("#")[0];
