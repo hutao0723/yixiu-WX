54bb90539e2f513e7e4250bc9cabdd44c623c567
keyexiang <keyexiang@duiba.com.cn>
2018-05-25 18:56:02 +0800

    root

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: '',
-    assetsPublicPath: '//yun.dui88.com/yoofans/knowledge',
+    assetsPublicPath: '//yun.dui88.com/yoofans/knowledge/',
     //yun.dui88.com/yoofans/konwledge
     productionSourceMap: false, // 设为true生成map可以作调试
     // Gzip off by default as many popular static hosts such as
