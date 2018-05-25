2e2d3311a79c1407029a285e65dbae8efbb2cdee
liugaoshuai <liugaoshuai@duiba.com.cn>
2018-05-25 15:09:54 +0800

    ahe

--- src/pages/knowledge/views/Help.vue
+++ src/pages/knowledge/views/Help.vue
@@ -43,10 +43,10 @@
     methods: {
       // 预览
       onImage() {
-        order.wxPreview('https://yun.duiba.com.cn/yoofans/images/201804/miniapp/ask-que.png');
+        order.wxPreview('https://yun.dui88.com/youfen/images/ask-que3.png');
       },
       onFlock() {
-        order.wxPreview('https://yun.duiba.com.cn/yoofans/images/201804/miniapp/ask-que2.png');
+        order.wxPreview('https://yun.dui88.com/youfen/images/ask-que4.png');
       },
     },
     mixins: [access]
