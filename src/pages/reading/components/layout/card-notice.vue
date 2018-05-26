390c78a2057fa7e02115d51956f11305fa33752e
ouyangchaoqun <ouyang123>
2018-05-26 14:55:56 +0800

    打卡日历

---
+++ src/pages/reading/components/layout/card-notice.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="cardNotice-box" >
+    <div class="notice-box">
+      <div class="notice-title">打卡须知</div>
+      <ul class="notice-content">
+        <div class="notice-order">
+          <span>1</span>
+          <i v-for="item in [1,2,3,4,5,6,7,8]"></i>
+          <span>2</span>
+        </div>
+        <li>点击xx页面，完成当日学习目标后，将自动弹出弹窗提醒打卡。或者在收听页面，点击“打卡“按钮进行打卡</li>
+        <li>分享打卡图片至微信朋友圈，并把分享截图发送至班级群，坚持49天即可获得一修阅读定制音响</li>
+      </ul>
+      <div class="notice-btn" @click="hideNotice()">我知道了</div>
+    </div>
+  </div>
+</template>
+<script>
+
+  export default {
+    props: ['noticeFlag'],
+    data () {
+      return {
+
+      };
+    },
+    methods: {
+      hideNotice(){
+        this.$emit('changFlag',false)
+      }
+    },
+    components: {
+
+    }
+  }
+</script>
+
+<style  lang="less">
+  @import "../../less/variable";
+  .cardNotice-box{
+    width:100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.6);
+    z-index: 999;
+    position: absolute;
+    top:0;
+    left:0;
+    color:#333;
+    .notice-box{
+      width:536/@rem;
+      border-radius: 8/@rem;
+      position: absolute;
+      top:50%;
+      left:50%;
+      transform: translate(-50%,-50%);
+      overflow: hidden;
+      .notice-title{
+        font-size: 30/@rem;
+        text-align: center;
+        height: 90/@rem;
+        line-height: 90/@rem;
+        background: #FFE555;
+      }
+      .notice-content{
+        position: relative;
+        background: #fff;
+        padding: 57/@rem 30/@rem 33/@rem 103/@rem;
+        li{
+          margin-bottom: 45/@rem;
+          color:#555555;
+          font-size: 24/@rem;
+          line-height: 37/@rem;
+        }
+        .notice-order{
+          position: absolute;
+          left:39/@rem;
+          span{
+            width:44/@rem;
+            height:44/@rem;
+            background: #FFE555;
+            border-radius: 50%;
+            display: block;
+            text-align: center;
+            line-height: 44/@rem;
+            margin-bottom: 9/@rem;
+          }
+          i{
+            width:4/@rem;
+            height:4/@rem;
+            background: #F7DC7B;
+            border-radius: 50%;
+            display: block;
+            margin: 0 auto;
+            margin-bottom: 9/@rem;
+          }
+        }
+      }
+      .notice-btn{
+        text-align: center;
+        border-top: 1px solid #E5E5E5;
+        height:82/@rem;
+        line-height: 82/@rem;
+        font-size: 28/@rem;
+        background: #fff;
+      }
+      .notice-btn:active{
+        background: #E5E5E5;
+      }
+    }
+  }
+</style>
--- src/pages/reading/views/card/Card.vue
+++ src/pages/reading/views/card/Card.vue
@@ -1,50 +1,208 @@
 <template>
   <div class="card-main">
-    打卡日历
+    <cardNotice class="hideNoticeStyle" :class="{showNoticeStyle:noticeFlag}" :noticeFlag="noticeFlag" @changFlag="changeFlag"></cardNotice>
+    <header>
+      <div>100天魔鬼训练营第3期</div>
+      <h2>第3天</h2>
+      <img src="" alt="">
+    </header>
+    <div class="card-head">
+      <span class="head-left" @click="noticeFlag = true ">坚持打卡送大礼 ></span>
+      <div class="head-right">
+        <span><i></i> 已打卡</span>
+        <span><i></i>未打卡</span>
+      </div>
+    </div>
+    <div class="week-box">
+      <span v-for="week in weeks" class="week">{{week}}</span>
+    </div>
+    <div class="calendar-box">
+      <calendar ></calendar>
+    </div>
+    <div class="book-book">
+      <div class="book-img"></div>
+      <div class="book-detail">
+        <div class="book-title">《这是一本书就是这么有意思》</div>
+        <div class="book-author">
+          <span>作者</span>
+          <!--<span class="book-btn">查看</span>-->
+          <span class="book-btn" @click="goComment()">写想法</span>
+        </div>
+      </div>
+      <div style="clear: both"></div>
+    </div>
+    <bnav></bnav>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-export default {
-  components: {
-  },
-  data () {
-    return {
-    };
-  },
-  computed: {
-    ...mapState({})
-  },
-  created() {
+  import { mapState } from 'vuex';
+  import bnav from '../../components/basic/Nav';
+  import calendar from '../../components/layout/calendar.vue';
+  import cardNotice from '../../components/layout/card-notice';
+  export default {
+    components: {
+      bnav,
+      calendar,
+      cardNotice,
     },
-  mounted () {
-  },
-  methods: {
-  }
-};
+    data () {
+      return {
+        noticeFlag:false,
+        // 自定义星期名称
+        weeks: ['日', '一', '二', '三', '四', '五', '六'],
+      };
+    },
+    computed: {
+      ...mapState({})
+    },
+    created() {
+
+    },
+    mounted () {
+
+    },
+    methods: {
+      changeFlag(msg){
+        this.noticeFlag = msg;
+      },
+      goComment(){
+        //发表想法
+        this.$router.push('/comment')
+      }
+    }
+  };
 </script>
 
 <style lang="less">
-@import '../../less/variable';
-.card-main {
-  width: 750/@rem;
-  height: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  overflow-x: hidden;
-  overflow-y: auto;
-  -webkit-overflow-scrolling: touch;
-  padding-top: 20/@rem;
-  // z-index: 9;
-  background: #fff;
-  font-size: 24/@rem;
-  .icon-nav{
-    height: 480/@rem;
-    border: 1px solid #ccc;
+  @import '../../less/variable';
+  .card-main {
+    width: 750/@rem;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    overflow-x: hidden;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;
+    // z-index: 9;
+    background: #f4f4f4;
+    font-size: 24/@rem;
+    color:#333;
+    .hideNoticeStyle{
+      opacity: 0;
+      z-index: -1;
+      transition:all .3s ease ;
+      -webkit-transition:all .3s ease ;
+    }
+    .showNoticeStyle{
+      opacity:1;
+      z-index:999;
+      transition:all .3s ease ;
+      -webkit-transition:all .3s ease ;
+    }
+    header{
+      padding:27/@rem 0 13/@rem 27/@rem;
+      border:1px solid #E5E5E5;
+      position: relative;
+      background: #fff;
+      div{
+        font-size: 30/@rem;
+        line-height: 42/@rem;
+      }
+      h2{
+        font-size: 50/@rem;
+        line-height: 70/@rem;
+      }
+      img{
+        position: absolute;
+        width:88/@rem;
+        height:88/@rem;
+        border-radius: 50%;
+        background: pink;
+        top:50%;
+        margin-top:-44/@rem;
+        right:54/@rem;
+      }
+    }
+    .week-box{
+      background: #fff;
+      span{
+        width:14.2857%;
+        display: inline-block;
+        text-align: center;
+        color:#666;
+        font-size: 26/@rem;
+        line-height: 37/@rem;
+        padding-bottom: 30/@rem;
+      }
+    }
+    .card-head{
+      font-size: 26/@rem;
+      padding:25/@rem 0 26/@rem 30/@rem;
+      line-height: 37/@rem;
+      background: #fff;
+      .head-right{
+        float: right;
+        font-size: 24/@rem;
+        color:#666;
+        line-height: 33/@rem;
+        span{
+          margin-right: 35/@rem;
+        }
+        span:nth-of-type(2) i{
+          background: #E8E8E8;
+        }
+        i{
+          width:20/@rem;
+          height:20/@rem;
+          background: #9688FF;
+          border-radius: 50%;
+          display: inline-block;
+          margin-right: 10/@rem;
+        }
+      }
+    }
+    .calendar-box{
+      margin-bottom: 18/@rem;
+      height:430/@rem;
+      overflow: auto;
+    }
+    .book-book{
+      padding:42/@rem 30/@rem 49/@rem 36/@rem;
+      background: #fff;
+      position: relative;
+      .book-img{
+        width:122/@rem;
+        height:165/@rem;
+        margin-right: 18/@rem;
+        background: pink;
+        float: left;
+      }
+      .book-detail{
+        padding-top: 10/@rem;
+        float: left;
+        .book-title{
+          font-size: 30/@rem;
+          line-height: 42/@rem;
+          margin-bottom: 45/@rem;
+        }
+        .book-author{
+          font-size: 26/@rem;
+          line-height: 37/@rem;
+          color:#666;
+          padding-left: 17/@rem;
+          .book-btn{
+            background: #FFE555;
+            padding:8/@rem 39/@rem;
+            border-radius: 25/@rem;
+            position: absolute;
+            right:30/@rem;
+          }
+        }
+      }
+    }
   }
-}
 </style>
 
