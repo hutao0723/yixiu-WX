23fa445642d8d00536594e1168476f5e01e4badd
liugaoshuai <liugaoshuai@duiba.com.cn>
2018-06-01 11:27:09 +0800

    ahe

--- src/pages/reading/api/order.js
+++ src/pages/reading/api/order.js
@@ -76,6 +76,7 @@ export default class order extends base {
         },
         function (res) {
           // if (res.err_msg == "get_brand_wcpay_request:ok") {
+          
           // } 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
 
           function url_add_hash(url, key) {
@@ -101,6 +102,7 @@ export default class order extends base {
               }
             }
           }
+          alert('支付成功' + url_add_hash(window.location.href))
 
           window.location.href = url_add_hash(window.location.href)
         }
