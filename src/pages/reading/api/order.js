3c241748e31381e919130b7207bb1ba0acfaabdb
liugaoshuai <liugaoshuai@duiba.com.cn>
2018-06-01 12:05:51 +0800

    ahe

--- src/pages/reading/api/order.js
+++ src/pages/reading/api/order.js
@@ -79,7 +79,13 @@ export default class order extends base {
 
           // } 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
           alert('跳转地址！')
-          window.location.href = '/reading.html#/index/home'
+          
+          const url = location.href
+          if (url.indexOf('?') > -1) {
+            location.href = url + '&timestamp=' + ((new Date()).getTime() + Math.random());
+          } else {
+            location.href = url + '?timestamp=' + ((new Date()).getTime() + Math.random());
+          }
         }
       );
     }
