00b8d31379f1c77357fd96f1b29b2b153500d33d
liugaoshuai <liugaoshuai@duiba.com.cn>
2018-06-01 11:56:09 +0800

    ahe

--- src/pages/reading/api/order.js
+++ src/pages/reading/api/order.js
@@ -78,33 +78,12 @@ export default class order extends base {
           // if (res.err_msg == "get_brand_wcpay_request:ok") {
           
           // } 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-
-          function url_add_hash(url, key) {
-            var key = (key || 't') + '='; //默认是"t"  
-            var reg = new RegExp(key + '\\d+'); //正则：t=1472286066028  
-            var timestamp = +new Date();
-            if (url.indexOf(key) > -1) { //有时间戳，直接更新  
-              return url.replace(reg, key + timestamp);
-            } else { //没有时间戳，加上时间戳  
-              if (url.indexOf('\?') > -1) {
-                var urlArr = url.split('\?');
-                if (urlArr[1]) {
-                  return urlArr[0] + '?' + key + timestamp + '&' + urlArr[1];
-                } else {
-                  return urlArr[0] + '?' + key + timestamp;
-                }
-              } else {
-                if (url.indexOf('#') > -1) {
-                  return url.split('#')[0] + '?' + key + timestamp + location.hash;
-                } else {
-                  return url + '?' + key + timestamp;
-                }
-              }
-            }
+          const url = window.location.href
+          if (url.indexOf('?') > -1) {
+            window.location.href=url+'&timestamp='+((new Date()).getTime()+Math.random());
+          }else{
+            window.location.href=url+'?timestamp='+((new Date()).getTime()+Math.random());
           }
-          alert('支付成功' + url_add_hash(window.location.href))
-
-          window.location.href = url_add_hash(window.location.href)
         }
       );
     }
