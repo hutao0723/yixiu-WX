1f595a658788e2fd37e72cdbf6754b32b982c168
liugaoshuai <liugaoshuai@duiba.com.cn>
2018-06-01 10:52:39 +0800

    ahe

--- src/pages/reading/api/order.js
+++ src/pages/reading/api/order.js
@@ -76,6 +76,7 @@ export default class order extends base {
         },
         function (res) {
           // if (res.err_msg == "get_brand_wcpay_request:ok") {
+            alert('支付成功' + location.href)
             location.reload()
           // } 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
         }
