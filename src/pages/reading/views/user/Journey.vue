5f317bb02450af8259875aa36d09cee8481406f8
hutao <hutao@duiba.com.cn>
2018-06-23 10:53:23 +0800

    提交测试

--- src/pages/reading/views/user/Journey.vue
+++ src/pages/reading/views/user/Journey.vue
@@ -216,6 +216,7 @@ export default {
       this.$set(row,'expand', this.expandStatus[row.id])
     },
     goPoster(item){
+        alert(item.posterType)
       if(item.posterType == "H5"){
         this.$router.push({name:'share',query:{commentId:item.id,lastClock:0,isClock:1}})
       }else{
